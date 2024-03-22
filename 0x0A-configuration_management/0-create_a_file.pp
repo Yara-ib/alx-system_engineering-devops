275d7f5035a0e9443fd4200dd6d52cdb5b057ed8
Yara-ib <yr.ap.23@gmail.com>
2024-03-22 20:25:26 +0200

    Creating File Manifest

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Manifest to create a file in tmp
+
+file {'school':
+    ensure  => present,
+    path    => '/tmp/school',
+    owner   => www-data,
+    group   => www-data,
+    mode    => '0744',
+    content => 'I love Puppet'
+}
