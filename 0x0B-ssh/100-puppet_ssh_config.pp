73da77ac17c0f7c2f48ee56f3df6de658e02b4f1
Yara-ib <yr.ap.23@gmail.com>
2024-03-25 04:22:21 +0200

    Puppet manifest

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 #  Manifest make changes to our configuration file.
 
 exec { 'ssh':
-    command  => 'ssh -F ~/.ssh/school -o PasswordAuthentication',
+    command  => 'ssh -F ~/.ssh/school -o PasswordAuthentication=no',
     provider => 'shell'
 }
