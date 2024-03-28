a2a88d6b951ff797dcc2057b32e18677bfad4462
Yara-ib <yr.ap.23@gmail.com>
2024-03-28 04:50:28 +0200

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,17 @@
 #  Manifest make changes to our configuration file.
 
+include stdlib
+
 file_line { 'Turn off password authentication':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true
+}
+
+file_line { 'Configure private key':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true
 }
