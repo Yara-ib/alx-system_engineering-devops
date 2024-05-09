a9e4ca192b077027e0c97473aa6c097d3bbf16c8
Yara-ib <yr.ap.23@gmail.com>
2024-05-09 03:00:43 +0300

    Strace could be a friend or foe

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'swap_phpp_php':
-    command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
     provider => shell
 }
