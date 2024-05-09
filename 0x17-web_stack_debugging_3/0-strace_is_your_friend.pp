915c6ee18b18b5446a9f52fda01cd4f3c80f121f
Yara-ib <yr.ap.23@gmail.com>
2024-05-09 03:01:42 +0300

    Strace could be a friend or foe

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
+# Using strace fixing why Apache is returning a 500 error
+
 exec { 'swap_phpp_php':
-    command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+    command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
     provider => shell
 }
