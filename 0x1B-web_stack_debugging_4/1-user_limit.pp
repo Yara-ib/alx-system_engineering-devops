a872050b5f1dcfd843c144d45e237ea5912b04d5
Yara-ib <yr.ap.23@gmail.com>
2024-05-16 22:17:14 +0300

    User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# User limit
+exec { 'user_limit_soft':
+    command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+    provider => shell
+}
+exec { 'user_limit_hard':
+    command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+    provider => shell
+}
