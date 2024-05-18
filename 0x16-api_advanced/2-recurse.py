797017a0414609194719806cd550c49bc6394f21
Yara-ib <yr.ap.23@gmail.com>
2024-05-18 19:29:54 +0300

    Recurse it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module for queries the Reddit API, parses the title of all hot articles """
+import requests
+
+
+def recurse(subreddit, hot_titles=[], after=''):
+    """ Function that queries the Reddit API, parses title of hot articles """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?after={after}'
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    if response.history or response.status_code == 404:
+        return None
+    response = response.json()
+    after = response.get('data').get('after')
+    if after:
+        response = response.get('data').get('children')
+        for title in response:
+            title = title.get('data').get('title')
+            hot_titles.append(title)
+        return recurse(subreddit, hot_titles, after)
+    else:
+        return hot_titles
