58a78e51cc193e4ba9529682ee5a2383643a8f03
Yara-ib <yr.ap.23@gmail.com>
2024-05-18 19:04:57 +0300

    Subscribers Count

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Number of subscribers Module """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Getting the number of subscribers """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom-user-agent'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
