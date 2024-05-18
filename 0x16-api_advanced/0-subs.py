fefe58ae3daad2d36522564ca001a2686a4553f2
Yara-ib <yr.ap.23@gmail.com>
2024-05-18 19:15:11 +0300

    Count not accurate

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Getting the number of subscribers """
+    """ Getting the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom-user-agent'}
+    headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
